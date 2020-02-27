9d919c7598ff25dc5bce3963ecd1f1a1591a0cfa
niskb <niskb19@gmail.com>
2020-02-26 23:05:44 -0500

    Initial Spring Boot commit
    Modified default banner ASCII text to my name in ASCII text. :smiley:

---
+++ app/src/main/java/niskb/ghostkitchen/GhostkitchenApplication.java
@@ -0,0 +1,13 @@
+package niskb.ghostkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostkitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostkitchenApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/niskb/ghostkitchen/GhostkitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package niskb.ghostkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostkitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
