32c1f591b9fe89ff2d7b08761a54c192aee104c4
niskb <niskb19@gmail.com>
2020-05-09 15:46:53 -0400

    Add Refresh Button to Navigation Bar
    Use it to refresh the page you're currently on.

--- react_app/reactjs/src/components/NavigationBar.js
+++ react_app/reactjs/src/components/NavigationBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { Navbar, Nav, Image } from 'react-bootstrap';
+import { Navbar, Nav, Image, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRecycle } from '@fortawesome/free-solid-svg-icons'
+
 export default function NavigationBar() {
+
     return (
         <Navbar bg="dark" variant="dark">
             <Link to="" className="navbar-brand"><Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgNZ0B0oxAKMmM3cttlOl2Kg7jvIR0cf29sHVwXbqmqZJd-cX4yw&s" roundedCircle width="76" height="76" alt="brand" /> Home</Link>
@@ -13,6 +17,11 @@ export default function NavigationBar() {
                 <Link to="users" className="nav-link">User List</Link>
                 <Link to="order" className="nav-link">Order Meals</Link>
             </Nav>
+            <Button type="button" variant="outline-info"
+                onClick={() => window.location.reload()}>
+                <FontAwesomeIcon icon={faRecycle} /> Refresh Page
+            </Button>
         </Navbar>
-        );
+    );
+
 }
